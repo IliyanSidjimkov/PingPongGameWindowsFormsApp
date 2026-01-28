782cb0e62693ab52a94b037717e1b9b125cb75a4
IliyanSidjimkov <sidjimkov_1979@abv.bg>
2026-01-28 11:45:07 +0200

    Add project files.

---
+++ Ping Pong Game/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace Ping_Pong_Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            player = new PictureBox();
+            computer = new PictureBox();
+            ball = new PictureBox();
+            GameTimer = new System.Windows.Forms.Timer(components);
+            menuStrip1 = new MenuStrip();
+            languageToolStripMenuItem = new ToolStripMenuItem();
+            bulgarianToolStripMenuItem = new ToolStripMenuItem();
+            englishToolStripMenuItem = new ToolStripMenuItem();
+            aboutToolStripMenuItem = new ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)player).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)computer).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)ball).BeginInit();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // player
+            // 
+            player.Image = Properties.Resources.Player;
+            player.Location = new Point(12, 148);
+            player.Name = "player";
+            player.Size = new Size(30, 120);
+            player.SizeMode = PictureBoxSizeMode.StretchImage;
+            player.TabIndex = 0;
+            player.TabStop = false;
+            // 
+            // computer
+            // 
+            computer.Image = Properties.Resources.computer;
+            computer.Location = new Point(758, 157);
+            computer.Name = "computer";
+            computer.Size = new Size(30, 120);
+            computer.SizeMode = PictureBoxSizeMode.StretchImage;
+            computer.TabIndex = 1;
+            computer.TabStop = false;
+            // 
+            // ball
+            // 
+            ball.Image = Properties.Resources.icons8_beach_ball_94;
+            ball.Location = new Point(374, 208);
+            ball.Name = "ball";
+            ball.Size = new Size(30, 30);
+            ball.SizeMode = PictureBoxSizeMode.StretchImage;
+            ball.TabIndex = 2;
+            ball.TabStop = false;
+            // 
+            // GameTimer
+            // 
+            GameTimer.Enabled = true;
+            GameTimer.Interval = 20;
+            GameTimer.Tick += GameTimerEvent;
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Dock = DockStyle.None;
+            menuStrip1.Items.AddRange(new ToolStripItem[] { languageToolStripMenuItem, aboutToolStripMenuItem });
+            menuStrip1.Location = new Point(350, 9);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(251, 24);
+            menuStrip1.TabIndex = 3;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // languageToolStripMenuItem
+            // 
+            languageToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { bulgarianToolStripMenuItem, englishToolStripMenuItem });
+            languageToolStripMenuItem.Name = "languageToolStripMenuItem";
+            languageToolStripMenuItem.Size = new Size(71, 20);
+            languageToolStripMenuItem.Text = "Language";
+            // 
+            // bulgarianToolStripMenuItem
+            // 
+            bulgarianToolStripMenuItem.Name = "bulgarianToolStripMenuItem";
+            bulgarianToolStripMenuItem.Size = new Size(180, 22);
+            bulgarianToolStripMenuItem.Text = "Bulgarian";
+            bulgarianToolStripMenuItem.Click += bulgarianToolStripMenuItem_Click;
+            // 
+            // englishToolStripMenuItem
+            // 
+            englishToolStripMenuItem.Name = "englishToolStripMenuItem";
+            englishToolStripMenuItem.Size = new Size(180, 22);
+            englishToolStripMenuItem.Text = "English";
+            englishToolStripMenuItem.Click += englishToolStripMenuItem_Click;
+            // 
+            // aboutToolStripMenuItem
+            // 
+            aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            aboutToolStripMenuItem.Size = new Size(52, 20);
+            aboutToolStripMenuItem.Text = "About";
+            aboutToolStripMenuItem.Click += aboutToolStripMenuItem_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Black;
+            ClientSize = new Size(800, 450);
+            Controls.Add(ball);
+            Controls.Add(computer);
+            Controls.Add(player);
+            Controls.Add(menuStrip1);
+            DoubleBuffered = true;
+            MainMenuStrip = menuStrip1;
+            Name = "Form1";
+            Text = "Player: 0 --- Computer: 0";
+            KeyDown += KeyIsDown;
+            KeyUp += KeyIsUp;
+            ((System.ComponentModel.ISupportInitialize)player).EndInit();
+            ((System.ComponentModel.ISupportInitialize)computer).EndInit();
+            ((System.ComponentModel.ISupportInitialize)ball).EndInit();
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox player;
+        private PictureBox computer;
+        private PictureBox ball;
+        private System.Windows.Forms.Timer GameTimer;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem languageToolStripMenuItem;
+        private ToolStripMenuItem bulgarianToolStripMenuItem;
+        private ToolStripMenuItem englishToolStripMenuItem;
+        private ToolStripMenuItem aboutToolStripMenuItem;
+    }
+}
---
+++ Ping Pong Game/Form1.cs
@@ -0,0 +1,251 @@
+﻿namespace Ping_Pong_Game
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+
+        string currentLanguage = "EN";
+        int ballXspeed = 4;
+        int ballYspeed = 4;
+        int speed = 2;
+        Random rand = new Random();
+        bool goDown, goUp;
+        int computerSpeedChange = 50;
+        int playerScore = 0;
+        int computerScore = 0;
+        int playerSpeed = 8;
+        int[] i = { 5, 6, 8, 9 };
+        int[] j = { 10, 9, 8, 11, 12 };
+
+
+        private void GameTimerEvent(object sender, EventArgs e)
+        {
+
+            ball.Top -= ballYspeed;
+            ball.Left -= ballXspeed;
+
+            if (currentLanguage == "BG")
+            {
+                this.Text = $"Играч: {playerScore} --- Компютър: {computerScore}";
+            }
+            else
+            {
+                this.Text = $"Player: {playerScore} --- Computer: {computerScore}";
+            }
+            if (ball.Top < 0 || ball.Bottom > this.ClientSize.Height)
+            {
+                ballYspeed = -ballYspeed;
+
+            }
+            if (ball.Left < -2)
+            {
+                ball.Left = 300;
+                ballXspeed = -ballXspeed;
+                computerScore++;
+            }
+            if (ball.Right > this.ClientSize.Width + 2)
+            {
+                ball.Left = 300;
+                ballXspeed = -ballXspeed;
+                playerScore++;
+            }
+            if (computer.Top <= 1)
+            {
+                computer.Top = 0;
+
+            }
+            else if (computer.Bottom >= this.ClientSize.Height)
+            {
+                computer.Top = this.ClientSize.Height - computer.Height;
+            }
+
+            if (ball.Top < computer.Top + (computer.Height / 2) && ball.Left > 300)
+            {
+                computer.Top -= speed;
+            }
+            if (ball.Top > computer.Top + (computer.Height / 2) && ball.Left > 300)
+            {
+                computer.Top += speed;
+
+            }
+
+            computerSpeedChange -= 1;
+
+            if (computerSpeedChange < 0)
+            {
+                speed = i[rand.Next(i.Length)];
+                computerSpeedChange = 50;
+            }
+            if (goDown && player.Top + player.Height < this.ClientSize.Height)
+            {
+                player.Top += playerSpeed;
+            }
+            if (goUp && player.Top > 0)
+            {
+                player.Top -= playerSpeed;
+            }
+            CheckCollison(ball, player, player.Right + 5);
+            CheckCollison(ball, computer, computer.Left - 35);
+            if (computerScore > 1)
+            {
+                GameOver(false);
+            }
+            else if (playerScore > 1)
+            {
+                GameOver(true);
+            }
+
+
+        }
+
+        private void KeyIsDown(object sender, KeyEventArgs e)
+        {
+
+            if (e.KeyCode == Keys.Down)
+            {
+                goDown = true;
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                goUp = true;
+            }
+
+        }
+
+        private void KeyIsUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Down)
+            {
+                goDown = false;
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                goUp = false;
+            }
+        }
+
+
+        private void CheckCollison(PictureBox PicOne, PictureBox PicTwo, int offset)
+        {
+
+            if (PicOne.Bounds.IntersectsWith(PicTwo.Bounds))
+            {
+                PicOne.Left = offset;
+                int x = j[rand.Next(j.Length)];
+                int y = j[rand.Next(j.Length)];
+                if (ballXspeed < 0)
+                {
+                    ballXspeed = x;
+
+                }
+                else
+                {
+                    ballXspeed = -x;
+                }
+
+                if (ballYspeed < 0)
+                {
+                    ballYspeed = -y;
+                }
+                else
+                {
+                    ballYspeed = -y;
+                }
+            }
+
+        }
+
+
+        private void GameOver(bool PlayerWon)
+        {
+            GameTimer.Stop();
+
+            string message = "";
+            string title = "";
+
+            if (currentLanguage == "BG")
+            {
+                title = "Край на играта";
+                message = PlayerWon
+                    ? "Ти спечели играта!"
+                    : "Съжаляваме, загуби играта.";
+            }
+            else
+            {
+                title = "Game Over";
+                message = PlayerWon
+                    ? "You won this game!"
+                    : "Sorry, you lost the game.";
+            }
+
+            MessageBox.Show(message, title);
+
+            computerScore = 0;
+            playerScore = 0;
+            ballXspeed = ballYspeed = 4;
+            computerSpeedChange = 50;
+            GameTimer.Start();
+
+        }
+
+        private void bulgarianToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            currentLanguage = "BG";
+            languageToolStripMenuItem.Text = "Език";
+            bulgarianToolStripMenuItem.Text = "Български"; 
+            englishToolStripMenuItem.Text = "Английски";
+            aboutToolStripMenuItem.Text = "Относно";
+        }
+
+        private void englishToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            currentLanguage = "EN";
+            languageToolStripMenuItem.Text = "Language";
+            bulgarianToolStripMenuItem.Text = "Bulgarian";
+            englishToolStripMenuItem.Text = "English";
+            aboutToolStripMenuItem.Text = "About";
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ShowAbout();
+        }
+        private void ShowAbout()
+        {
+            GameTimer.Stop();
+            string title = "";
+            string message = "";
+
+            if (currentLanguage == "BG")
+            {
+                title = "Относно играта";
+                message =
+                    "Име на проекта: Pong Game\n" +
+                    "Тип: Windows Forms Application\n\n" +
+                    "Разработчик: Илиян Сиджимков\n" +
+                    "Факултетен №: Ф112803\n" +
+                    "Класическа Pong игра с управление от клавиатурата " +
+                    "и поддръжка на български и английски език.";
+            }
+            else // EN
+            {
+                title = "About the game";
+                message =
+                    "Project name: Pong Game\n" +
+                    "Type: Windows Forms Application\n\n" +
+                    "Developer: Iliyan Sidzhimkov\n" +
+                    "Faculty number: F112803\n" +
+                    "Classic Pong game with keyboard control " +
+                    "and support for Bulgarian and English language.";
+            }
+            
+            MessageBox.Show(message, title, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            GameTimer.Start();
+        }
+    }
+}
